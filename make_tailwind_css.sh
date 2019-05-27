03bfb2a77624247fb932bcdf857f7840b8ddd2f1
iancleary <iancleary@pm.me>
2019-05-27 15:45:42 -0700

    initial helper script

---
+++ make_tailwind_css.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+###############################################
+# Utility to create tailwind files
+# - could easily inspect the css/tailwind_css folder 
+###############################################
+
+npx tailwind build css/tailwind_css/main.css -o css/main.css
