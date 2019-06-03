fe0ca195be238852192160a382c6c7c3c1c52919
iancleary <iancleary@pm.me>
2019-06-03 07:27:02 -0700

    initial python script

---
+++ update_site.py
@@ -0,0 +1,7 @@
+# do not inlcude python interpreter as this will be called
+# directly with the update_site.sh bash script
+# with a venv sourced already
+
+import boto3
+
+print("Hello from script")
--- update_site.sh
+++ update_site.sh
@@ -7,13 +7,31 @@
 ###############################################
 
 TMP="/tmp"
+
 GIT_USER="iancleary"
 GIT_REPO="iancleary.me"
+GIT_PATH="$GIT_USER/$GIT_REPO.git"
 TMP_LOCATION="$TMP/$GIT_REPO"
+
+PYTHON_FILE_FULLPATH="$TMP_LOCATION/update_site.py"
 TMP_VENV_LOCATION="$TMP_LOCATION/venv"
+
 VENV_BIN_ACTIVATE_SUBPATH="bin/activate"
 VENV_SOURCE_PATH="$TMP_VENV_LOCATION/$VENV_BIN_ACTIVATE_SUBPATH"
-GIT_PATH="$GIT_USER/$GIT_REPO.git"
+
+# echo ${TMP:0:4}
+
+if [[ ${TMP:0:4} == '/tmp' ]]
+then
+    echo "$TMP location is indeed within /tmp (safe to delete)"
+    rm -rf $TMP_LOCATION
+else
+    echo "$TMP location is not within /tmp"
+    echo "--------------------------------------------"
+    echo "exiting script, not deleting outside of /tmp"
+    echo "--------------------------------------------"
+    exit 1
+fi
 
 echo -n "Enter the branch to use and press [ENTER]; none for master: "
 read branch
@@ -23,10 +41,6 @@ if [ $branch = ""]; then
 fi
 
 echo "Using branch: $branch"
-
-#exit 1
-
-rm -rf $TMP_LOCATION
 
 echo "Cloning repo to $TMP directory."
 git clone -b $branch git@github.com:$GIT_PATH $TMP_LOCATION || git clone -b $branch https://github.com/$GIT_PATH $TMP_LOCATION
@@ -44,3 +58,5 @@ pip3 install -r "$TMP_LOCATION/requirements.txt"
 # - update CloudFront to force invalidation?
 # - celebrate!
 pip3 list
+
+python3 $TMP_LOCATION/update_site.py
