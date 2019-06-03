98a657d1b21c511b1f1e595b6deb7e1f4c54d562
iancleary <iancleary@pm.me>
2019-06-03 07:11:59 -0700

    working up to pip3 install

--- update_site.sh
+++ update_site.sh
@@ -2,15 +2,18 @@
 
 ###############################################
 # Utility to update AWS S3 with the latest version of this code
-# - prompts user to 
-# - creates a venv in /tmp to remove clutter and not install globally
+# - prompts user for branch to deploy (defaults to master)
+# - creates a venv in /tmp to not install globally (/tmp gets cleared as well)
 ###############################################
 
-TMP_LOCATION="/tmp/iancleary.me"
+TMP="/tmp"
+GIT_USER="iancleary"
+GIT_REPO="iancleary.me"
+TMP_LOCATION="$TMP/$GIT_REPO"
 TMP_VENV_LOCATION="$TMP_LOCATION/venv"
 VENV_BIN_ACTIVATE_SUBPATH="bin/activate"
 VENV_SOURCE_PATH="$TMP_VENV_LOCATION/$VENV_BIN_ACTIVATE_SUBPATH"
-GIT_REPO="iancleary/iancleary.me.git"
+GIT_PATH="$GIT_USER/$GIT_REPO.git"
 
 echo -n "Enter the branch to use and press [ENTER]; none for master: "
 read branch
@@ -23,30 +26,13 @@ echo "Using branch: $branch"
 
 #exit 1
 
-git clone -b $branch git@github.com:$GIT_REPO /tmp || git clone -b $branch https://github.com/$GIT_REPO /tmp
+rm -rf $TMP_LOCATION
 
-# CWD="$(pwd)"
+echo "Cloning repo to $TMP directory."
+git clone -b $branch git@github.com:$GIT_PATH $TMP_LOCATION || git clone -b $branch https://github.com/$GIT_PATH $TMP_LOCATION
 
-# echo $CWD
-
-# if [ -d $TMP_LOCATION ] 
-# then
-#     echo "Directory $TMP_LOCATION exists."
-# else
-#     echo "Directory $TMP_LOCATION does not exists."
-#     echo "Creating $TMP_LOCATION directory."
-#     mkdir $TMP_LOCATION 
-# fi
-
-## Check if the tmp location of the venv exists
-if [ -d $TMP_VENV_LOCATION ] 
-then
-    echo "Using existing python3 virtualenv in $TMP_VENV_LOCATION."
-else
-    echo "Directory $TMP_VENV_LOCATION does not exists."
-    echo "Creating python3 virtualenv in $TMP_LOCATION."  
-    python3 -m venv $TMP_VENV_LOCATION
-fi
+echo "Creating python3 virtualenv in $TMP_VENV_LOCATION."
+python3 -m venv $TMP_VENV_LOCATION
 
 ## 
 echo "sourcing $VENV_SOURCE_PATH"
@@ -54,9 +40,7 @@ source $VENV_SOURCE_PATH
 pip3 install -r "$TMP_LOCATION/requirements.txt"
 
 ## TODO
-# - git clone directory from cwd or from git
-# - pip3 install requirements.txt
 # - run script using boto3 and credentials in .env file to update s3
 # - update CloudFront to force invalidation?
 # - celebrate!
-cd
+pip3 list
