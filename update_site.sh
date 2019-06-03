d5e6ba20aa59c8fe232f413a1969c0bddeabd1ca
iancleary <iancleary@pm.me>
2019-06-03 07:02:12 -0700

    updated to clone repo directly

--- update_site.sh
+++ update_site.sh
@@ -2,26 +2,41 @@
 
 ###############################################
 # Utility to update AWS S3 with the latest version of this code
+# - prompts user to 
+# - creates a venv in /tmp to remove clutter and not install globally
 ###############################################
 
 TMP_LOCATION="/tmp/iancleary.me"
 TMP_VENV_LOCATION="$TMP_LOCATION/venv"
 VENV_BIN_ACTIVATE_SUBPATH="bin/activate"
 VENV_SOURCE_PATH="$TMP_VENV_LOCATION/$VENV_BIN_ACTIVATE_SUBPATH"
+GIT_REPO="iancleary/iancleary.me.git"
 
-CWD="$(pwd)"
+echo -n "Enter the branch to use and press [ENTER]; none for master: "
+read branch
 
-echo $CWD
-
-## Check if the tmp location exists
-if [ -d $TMP_LOCATION ] 
-then
-    echo "Directory $TMP_LOCATION exists."
-else
-    echo "Directory $TMP_LOCATION does not exists."
-    echo "Creating $TMP_LOCATION directory."
-    mkdir $TMP_LOCATION 
+if [ $branch = ""]; then
+    branch="master"
 fi
+
+echo "Using branch: $branch"
+
+#exit 1
+
+git clone -b $branch git@github.com:$GIT_REPO /tmp || git clone -b $branch https://github.com/$GIT_REPO /tmp
+
+# CWD="$(pwd)"
+
+# echo $CWD
+
+# if [ -d $TMP_LOCATION ] 
+# then
+#     echo "Directory $TMP_LOCATION exists."
+# else
+#     echo "Directory $TMP_LOCATION does not exists."
+#     echo "Creating $TMP_LOCATION directory."
+#     mkdir $TMP_LOCATION 
+# fi
 
 ## Check if the tmp location of the venv exists
 if [ -d $TMP_VENV_LOCATION ] 
@@ -29,13 +44,14 @@ then
     echo "Using existing python3 virtualenv in $TMP_VENV_LOCATION."
 else
     echo "Directory $TMP_VENV_LOCATION does not exists."
-    echo "Creating python3 virtualenv in $TMP_LOCATION."
+    echo "Creating python3 virtualenv in $TMP_LOCATION."  
     python3 -m venv $TMP_VENV_LOCATION
 fi
 
-## Source python3 venv
+## 
 echo "sourcing $VENV_SOURCE_PATH"
 source $VENV_SOURCE_PATH
+pip3 install -r "$TMP_LOCATION/requirements.txt"
 
 ## TODO
 # - git clone directory from cwd or from git
@@ -43,3 +59,4 @@ source $VENV_SOURCE_PATH
 # - run script using boto3 and credentials in .env file to update s3
 # - update CloudFront to force invalidation?
 # - celebrate!
+cd
