cb51a709da89a837761e1ced2b610102aaf81da8
iancleary <iancleary@pm.me>
2019-09-11 19:08:36 -0700

    swapped outer div and hyperlink

--- src/components/LinkCard.vue
+++ src/components/LinkCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="content-center m-6 lg:m-2">
-    <div
-      class="cursor-pointer max-w-sm mx-auto flow p-6 hover:bg-gray-500 rounded-lg shadow-xl"
-      :class="link.color"
-    >
-      <a style="text-decoration : none" :href="link.url" target="_blank">
+    <a style="text-decoration : none" :href="link.url" target="_blank">
+      <div
+        class="cursor-pointer max-w-sm mx-auto flow p-6 hover:bg-gray-500 rounded-lg shadow-xl"
+        :class="link.color"
+      >
         <div class="link">
           <span class="block relative py-2">
             <i class="absolute top-0 right-0 fas fa-external-link-alt"></i>
@@ -14,8 +14,8 @@
             <p class="italic text-base text-black-600 leading-normal">{{ link.message }}</p>
           </span>
         </div>
-      </a>
-    </div>
+      </div>
+    </a>
   </div>
 </template>
 
