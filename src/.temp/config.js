c5da1e2f2d900dbb4083052678c1a4b4ee14301c
iancleary <iancleary@pm.me>
2019-09-21 18:22:11 -0700

    removed .temp

--- src/.temp/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "titleTemplate": "%s - Gridsome Portfolio Starter",
-  "siteUrl": "https://gridsome-portfolio-starter.netlify.com",
-  "version": "0.7.2"
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1569114717848
