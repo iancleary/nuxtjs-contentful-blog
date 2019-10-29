edfe9470d5d81a7212d6219ffdfa4f46000ca18f
Ian Cleary <iancleary@pm.me>
2019-10-28 21:53:05 -0700

    refactored contact to Vue

---
+++ src/pages/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <contact-me-form></contact-me-form>
+  </Layout>
+</template>
+
+<script>
+import ContactMeForm from "../components/ContactMeForm";
+
+export default {
+  metaInfo: {
+    title: "Ciao-Ian"
+  },
+  components: {
+    ContactMeForm
+  }
+};
+</script>
+
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -170,79 +170,8 @@
 
     <div class="overflow-x-hidden">
       <div class="contact-me bg-background-secondary pt-16">
-        <div class="container-inner mx-auto text-xl pb-4 relative">
-          <h2 class="font-bold mb-6" id="contact">Contact me:</h2>
-
-          <div class="absolute right-0 top-0" style="transform: translate(100%) rotate(180deg)">
-            <svg width="170px" height="170px">
-              <use xlink:href="#dots-triangle" />
-            </svg>
-          </div>
-
-          <div class="text-lg sm:text-lg mb-16">
-            <form
-              name="contact"
-              method="POST"
-              netlify-honeypot="bot-field"
-              data-netlify="true"
-              class="mb-12"
-            >
-              <div class="flex flex-wrap mb-6 -mx-4">
-                <div class="w-full md:w-1/2 mb-6 md:mb-0 px-4">
-                  <label class="block mb-2 text-copy-primary" for="name">Name</label>
-                  <p class="hidden">
-                    <label>
-                      Don’t fill this out if you're human:
-                      <input name="bot-field" />
-                    </label>
-                  </p>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Jon Snow"
-                    class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none focus:border-green-700 mb-2 p-4"
-                    required
-                  />
-                </div>
-
-                <div class="w-full px-4 md:w-1/2">
-                  <label class="block text-copy-primary mb-2" for="email">Email Address</label>
-
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="email@example.com"
-                    class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none focus:border-green-700 mb-2 p-4"
-                    required
-                  />
-                </div>
-              </div>
-
-              <div class="w-full mb-12">
-                <label class="block text-copy-primary mb-2" for="message">Message</label>
-
-                <textarea
-                  id="message"
-                  rows="5"
-                  name="message"
-                  class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-green-700 mb-2 px-4 py-4"
-                  placeholder="Enter your message here."
-                  required
-                ></textarea>
-              </div>
-
-              <div class="flex justify-end w-full">
-                <input
-                  type="submit"
-                  value="Submit"
-                  class="block bg-green-700 hover:bg-green-800 text-white text-sm font-semibold tracking-wide uppercase shadow rounded cursor-pointer px-6 py-3"
-                />
-              </div>
-            </form>
-          </div>
-        </div>
+        <contact-me-form>
+        </contact-me-form>
       </div>
       <!-- end contact-me -->
     </div>
@@ -385,9 +314,14 @@
 
 <script>
 
+import ContactMeForm from '../components/ContactMeForm'
+
 export default {
   metaInfo: {
     title: "Home"
+  },
+  components: {
+    ContactMeForm
   }
 };
 </script>
