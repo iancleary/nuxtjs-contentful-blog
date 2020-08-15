6e550c81bf0ee50d8b3d1fbac68713b7010bc2c5
iancleary <iancleary@hey.com>
2020-08-15 00:35:42 -0700

    Article Card

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="project-card">
+    <!-- eslint-disable -->
+    <div class="break-normal w-2/3 pb-2">
+      {{ title }}
+    </div>
+    <span class="text-lg text-gray-600" v-html="description" />
+    <!-- eslint-enable -->
+  </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: {
+            type: String,
+            default: "Blog Article's Title",
+        },
+        description: {
+            type: String,
+            default: "Blog Article Description",
+        },
+        tagList: {
+            type: Array[String],
+            default: () => ["Ubuntu", "Ansible"],
+        },
+    },
+};
+
+
+</script>
+
+<style>
+</style>
