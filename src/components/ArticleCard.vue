b060dc828a0b38ef93051ab4034160321553d696
iancleary <iancleary@hey.com>
2020-08-15 23:12:43 -0700

    Rewrote article list component

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="project-card">
+  <div class="blog-card">
     <!-- eslint-disable -->
-    <div class="break-normal w-2/3 pb-2">
-      {{ title }}
+    <div class="break-normal pb-2">
+      <a
+        :href="url"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Link to my blog post on Dev.to's website"
+      >{{ title }}</a>
+      <span class="text-sm text-gray-600"> {{ readablePublishDate }} </span>
     </div>
     <span class="text-lg text-gray-600" v-html="description" />
     <!-- eslint-enable -->
@@ -10,25 +16,30 @@
 </template>
 
 <script>
-
 export default {
-    props: {
-        title: {
-            type: String,
-            default: "Blog Article's Title",
-        },
-        description: {
-            type: String,
-            default: "Blog Article Description",
-        },
-        tagList: {
-            type: Array[String],
-            default: () => ["Ubuntu", "Ansible"],
-        },
+  props: {
+    title: {
+      type: String,
+      default: "Blog Article's Title",
     },
+    description: {
+      type: String,
+      default: "Blog Article Description",
+    },
+    tagList: {
+      type: Array[String],
+      default: () => ["Ubuntu", "Ansible"],
+    },
+    url: {
+      type: String,
+      default: "https://dev.to/iancleary/",
+    },
+    readablePublishDate: {
+      type: String,
+      default: "Dec 8 '19",
+    },
+  },
 };
-
-
 </script>
 
 <style>
