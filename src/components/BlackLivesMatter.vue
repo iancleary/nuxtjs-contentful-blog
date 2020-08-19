45e3b06b36db8784f285923301a9e06f73bd9aa6
iancleary <iancleary@hey.com>
2020-08-18 18:37:00 -0700

    Black Lives Matter banner

---
+++ src/components/BlackLivesMatter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center justify-center flex-wrap bg-black py-2">
+    <span class="text-white">#BlackLivesMatter</span>
+    <span class="pl-2 text-green">
+      <a
+        role="button"
+        href="https://support.eji.org/give/153413/#!/donation/checkout"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Support the Equal Justice Initiative"
+      >
+        Support the Equal Justice Initiative
+      </a>
+    </span>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
