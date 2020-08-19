7545d8dbf4179cf2cbec9501406b51554976ea4f
Ian Cleary <iancleary@hey.com>
2020-08-18 18:38:51 -0700

    🎉 Black Lives Matter banner (#141)

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -1,34 +1,37 @@
 <template>
-  <header
-    class="border-t-8 border-green-700 py-4 sticky top-0 bg-background-primary opacity-90 z-10"
-  >
-    <div class="container pt-2 pb-8 inset-x-0">
-      <ul class="absolute left-0 flex items-center">
-        <li class="ml-8">
-          <g-link
-            to="/"
-            class="text-xl font-bold text-left"
-          >
-            Ian Cleary
-          </g-link>
-        </li>
-      </ul>
-      <ul class="absolute right-0 flex space-x-8 pr-8 items-center pt-1">
-        <li>
-          <ToggleTheme />
-        </li>
-        <li>
-          <DevLogo />
-        </li>
-        <li>
-          <GitHubLogo />
-        </li>
-        <li>
-          <TwitterLogo />
-        </li>
-      </ul>
-    </div>
-  </header>
+  <nav class="sticky top-0 pb-8 z-10">
+    <BlackLivesMatter />
+    <header
+      class=" bg-background-primary py-4 opacity-90"
+    >
+      <div class="container inset-x-0 pt-2 pb-8">
+        <ul class="absolute left-0 flex items-center">
+          <li class="ml-8">
+            <g-link
+              to="/"
+              class="text-xl font-bold text-left"
+            >
+              Ian Cleary
+            </g-link>
+          </li>
+        </ul>
+        <ul class="absolute right-0 flex space-x-8 pr-8 items-center pt-1">
+          <li>
+            <ToggleTheme />
+          </li>
+          <li>
+            <DevLogo />
+          </li>
+          <li>
+            <GitHubLogo />
+          </li>
+          <li>
+            <TwitterLogo />
+          </li>
+        </ul>
+      </div>
+    </header>
+  </nav>
 </template>
 
 <script>
@@ -37,6 +40,7 @@ import DevLogo from '@/components/DEVLogo.vue';
 import ToggleTheme from '@/components/ToggleTheme.vue';
 import TwitterLogo from '@/components/TwitterLogo.vue';
 import GitHubLogo from '@/components/GitHubLogo.vue';
+import BlackLivesMatter from '@/components/BlackLivesMatter.vue';
 
 export default {
   components: {
@@ -44,6 +48,7 @@ export default {
       DevLogo,
       GitHubLogo,
       TwitterLogo,
+      BlackLivesMatter,
   },
 };
 </script>
