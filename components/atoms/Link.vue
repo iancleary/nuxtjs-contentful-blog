e4c4dab88ff72c6a9e3435262a7fe59818504c90
iancleary <hello@iancleary.me>
2021-08-08 20:53:05 -0700

    WIP Link component

---
+++ components/atoms/Link.vue
@@ -0,0 +1,30 @@
+<template>
+  <a
+    role="button"
+    href="{{ href }}"
+    target="_blank"
+    rel="noopener noreferrer"
+    title="{{ title }}"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: "#",
+    },
+    title: {
+        type: String,
+        default: "Placeholder Title",
+    },
+    text: {
+        type: String,
+        default: "Placeholder Title",
+    },
+  },
+};
+</script>
