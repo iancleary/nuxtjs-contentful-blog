d0617e700a0bc5230719070996c6861b39d37ca0
iancleary <hello@iancleary.me>
2021-08-13 21:04:23 -0700

    Add Icon component to GitHubIcon

---
+++ components/atoms/Icon.vue
@@ -0,0 +1,53 @@
+<template>
+  <a
+    role="button"
+    :href="href"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="toggle-theme"
+    :title="title"
+    :alt="title"
+  >
+    <!-- https://tailwindcss.com/docs/fill#usage -->
+    <!-- eslint-disable-next-line -->
+    <slot
+      size="1.5x"
+      class="fill-current text-white h-8 w-8" 
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+
+
+.toggle-theme {
+  background-color: transparent;
+  border: 0;
+  color: black;
+  cursor: pointer;
+}
+
+.toggle-theme:hover {
+    opacity: 0.8;
+}
+
+.toggle-theme:focus {
+    outline: none;
+}
+
+</style>
--- components/molecules/GitHubIcon.vue
+++ components/molecules/GitHubIcon.vue
@@ -1,24 +1,24 @@
 <template>
-  <a
-    role="button"
-    href="https://github.com/iancleary"
-    target="_blank"
-    rel="noopener noreferrer"
-    class="toggle-theme"
-    title="Link to my GitHub Profile"
-  >
-    <!-- https://tailwindcss.com/docs/fill#usage -->
-    <!-- eslint-disable-next-line -->
-    <git-hub-icon size="1.5x" class="fill-current text-white h-8 w-8"></git-hub-icon>
-  </a>
+  <div>
+    <icon
+      href="https://github.com/iancleary"
+      title="Link to my GitHub Profile"
+    >
+      <!-- https://tailwindcss.com/docs/fill#usage -->
+      <!-- eslint-disable-next-line -->
+      <git-hub-icon size="1.5x" class="fill-current text-white h-8 w-8"></git-hub-icon>
+    </icon>
+  </div>
 </template>
 
 <script>
 import { GitHubIcon } from 'vue-simple-icons';
+import Icon from '@/components/atoms/Icon.vue';
 
 export default {
   components: {
     GitHubIcon,
+    Icon,
   },
 };
 </script>
