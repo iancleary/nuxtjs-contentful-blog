69e6899ffbf5b33f42a31ca75b710a1b6dc2cd97
icancclearynow <hello@iancleary.me>
2021-08-31 11:46:42 -0700

    #334: Fixed px between main index and article vue

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,32 +1,30 @@
 <template>
-  <div class="bg-background-primary pb-24 border-b-2 border-green-700">
-    <div class="relative pt-16">
-      <div class="relative mx-auto px-6 sm:px-6 lg:px-8">
-        <div class="text-lg max-w-xlg mb-6 p-6 bg-gray-100 rounded-lg shadow-lg">
-          <p class="text-center leading-6 text-gray-500 font-semibold tracking-wide uppercase">
-            Introducing
-          </p>
-          <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10">
-            {{ post.fields.title }}
-          </h1>
-          <div
-            class="image w-full rounded-lg"
-            :style="
-              `background: url(https:${post.fields.heroImage.fields.file.url}) center center no-repeat`
-            "
-          />
-          <!-- eslint-disable -->
+  <div class="relative pt-16">
+    <div class="relative sm:px-6 lg:px-8">
+      <div class="text-lg max-w-xlg mb-6 p-6 bg-gray-100 rounded-lg shadow-lg">
+        <p class="text-center leading-6 text-gray-500 font-semibold tracking-wide uppercase">
+          Introducing
+        </p>
+        <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10">
+          {{ post.fields.title }}
+        </h1>
+        <div
+          class="image w-full rounded-lg"
+          :style="
+            `background: url(https:${post.fields.heroImage.fields.file.url}) center center no-repeat`
+          "
+        />
+        <!-- eslint-disable -->
         <article class="prose prose-lg max-w-full text-gray-800" v-html="$md.render(post.fields.body)"/>
         <!-- eslint-enable -->
-          <p class="back justify-center">
-            <nuxt-link
-              exact
-              to="/"
-            >
-              ⟵ Back to Home
-            </nuxt-link>
-          </p>
-        </div>
+        <p class="back justify-center">
+          <nuxt-link
+            exact
+            to="/"
+          >
+            ⟵ Back to Home
+          </nuxt-link>
+        </p>
       </div>
     </div>
   </div>
