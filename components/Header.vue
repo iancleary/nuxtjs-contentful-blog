1c5867bb30d0f7d1b6da56409ad7a13a5f500b7c
icancclearynow <hello@iancleary.me>
2021-09-23 21:55:02 -0700

    Added navbar to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,31 @@
 <template>
-  <nav class="top-0 pb-8 z-10">
+  <nav class="z-10">
     <BlackLivesMatter />
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <header class="bg-indigo-600">
+      <nav
+        class="max-w-7xl mx-auto pt-6 px-4 sm:px-6 lg:px-8"
+        aria-label="Top"
+      >
+        <div class="w-full flex items-center justify-between">
+          <div class="flex items-center">
+            <a
+              href="/"
+              class="font-medium text-white"
+              key="Home"
+            >
+              Home
+            </a>
+          </div>
+          <div class="ml-10 space-x-4">
+            <a
+              href="/blog"
+              class="font-medium text-white"
+            >Blog</a>
+          </div>
+        </div>
+      </nav>
+    </header>
   </nav>
 </template>
 
