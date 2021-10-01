e71537202bcf8285cf1d0129f3f2f0de47b2ac57
icancclearynow <hello@iancleary.me>
2021-10-01 15:41:21 -0700

    Remove stale commented out html

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,24 +1,14 @@
 <template>
-  <!-- Overflow-x-hidden prevents horizontal scrolling when
-      elements are larger than others within the app -->
   <div>
     <LazyHydrate when-idle>
       <BlogSection />
     </LazyHydrate>
-    <!-- </LazyHydrate> -->
-    <!-- <LazyHydrate when-visible>
-      <Projects />
-    </LazyHydrate> -->
-    <!-- <LazyHydrate when-visible>
-      <PostImages />
-    </LazyHydrate> -->
   </div>
 </template>
 
 <script>
 
 import LazyHydrate from 'vue-lazy-hydration';
-import Hero from '@/components/Hero.vue';
 import BlogSection from '@/components/BlogSection.vue';
 
 
