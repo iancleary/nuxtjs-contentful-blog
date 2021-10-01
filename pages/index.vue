882584805697c597e70d14c32016b01e2a2a4206
icancclearynow <hello@iancleary.me>
2021-09-30 21:10:48 -0700

    Adjust github icon size, reorganize hero and remove external link list

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,28 @@
   <!-- Overflow-x-hidden prevents horizontal scrolling when
       elements are larger than others within the app -->
   <div>
-    <div class="border-b-2 border-green-700">
-      <Hero />
+    <!-- <div class="border-b-2 border-green-700"> -->
+    <Hero />
+    <!-- </div> -->
+    <div class="mb-4 mt-8">
+      <div class="container-inner mx-auto">
+        <div class="flex flex-col sm:flex-row justify-between items-center">
+          <div class="w-full sm:w-1/2 px-8">
+            <img
+              src="~/assets/unDraw/connectedWorld.svg"
+              alt="Connected world"
+              immediate="true"
+            >
+          </div>
+          <div class="w-full sm:w-1/2 px-8 pt-8 sm:py-0">
+            <img
+              src="~/assets/unDraw/lightTheFire.svg"
+              alt="woman next to illuminated lighthouse"
+              immediate="true"
+            >
+          </div>
+        </div>
+      </div>
     </div>
     <LazyHydrate when-idle>
       <BlogSection />
