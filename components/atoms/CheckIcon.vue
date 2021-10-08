89416cebd517c43aa668332603be7d8cbfcbe009
icancclearynow <hello@iancleary.me>
2021-10-07 18:14:06 -0700

    WIP

---
+++ components/atoms/CheckIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- https://tailwindcss.com/docs/fill#usage -->
+    <!-- eslint-disable-next-line -->
+      <div size="2.0x" class="fill-current text-white h-8 w-8">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
