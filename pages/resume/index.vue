8fafe116bd769d0a74a2601be02e36aa53cfad03
icancclearynow <hello@iancleary.me>
2021-10-19 21:53:38 -0700

    Fix prop name

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -7,7 +7,7 @@
         class="pt-8 flex justify-center"
       >
         <external-link
-          href="https://assets.ctfassets.net/b4dlpmwrdw4v/4f2HV8YDADVfEXrXYhcX5H/8087ede89c94932235650975f6159da7/IanClearyResume2021.pdf"
+          link="https://assets.ctfassets.net/b4dlpmwrdw4v/4f2HV8YDADVfEXrXYhcX5H/8087ede89c94932235650975f6159da7/IanClearyResume2021.pdf"
           alt="Ian Cleary's Resume as a PDF"
           css="border-2 border-gray-500 m-2 p-4 rounded-full font-bold text-copy-primary"
         >
