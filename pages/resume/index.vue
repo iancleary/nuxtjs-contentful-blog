ce5193befd51c4bbc12fb331aa8f8ada6c5b85a2
Ian Cleary (he/him/his) <contact@iancleary.me>
2021-10-19 21:55:47 -0700

    Move resume content (#363)

---
+++ pages/resume/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- Overflow-x-hidden prevents horizontal scrolling when
+      elements are larger than others within the app -->
+  <div>
+    <div class="pt-8">
+      <div
+        class="pt-8 flex justify-center"
+      >
+        <external-link
+          link="https://assets.ctfassets.net/b4dlpmwrdw4v/4f2HV8YDADVfEXrXYhcX5H/8087ede89c94932235650975f6159da7/IanClearyResume2021.pdf"
+          alt="Ian Cleary's Resume as a PDF"
+          css="border-2 border-gray-500 m-2 p-4 rounded-full font-bold text-copy-primary"
+        >
+          Download CV
+        </external-link>
+      </div>
+      <ExperienceList />
+    </div>
+    <!-- </LazyHydrate> -->
+    <!-- <LazyHydrate when-visible>
+      <Projects />
+    </LazyHydrate> -->
+    <!-- <LazyHydrate when-visible>
+      <PostImages />
+    </LazyHydrate> -->
+  </div>
+</template>
+
+<script>
+
+
+import LazyHydrate from 'vue-lazy-hydration';
+import ExperienceList from '@/components/molecules/ExperienceList.vue';
+import ExternalLink from '~/components/atoms/ExternalLink.vue';
+
+// import PostImages from '@/components/organisms/PostImages.vue';
+// import Projects from '@/components/organisms/Projects.vue';
+
+export default {
+  components: {
+    LazyHydrate,
+    ExperienceList,
+    ExternalLink,
+    // PostImages,
+    // Projects,
+  },
+  layout: 'default',
+  head: {
+    title: 'Ian Cleary 📡🛰',
+  },
+};
+
+</script>
